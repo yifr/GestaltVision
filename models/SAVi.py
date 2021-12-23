69501306d4a93ef037f3e7fda812493d25f08ed8
Yoni Friedman <yoniifriedman@gmail.com>
2021-12-23 04:41:41 -0500

    slot attention for video no size bugs?

--- models/SAVi.py
+++ models/SAVi.py
@@ -4,6 +4,104 @@ import torch.nn.functional as F
 import numpy as np
 
 
+def scaled_dot_product(q, k, v, mask=None):
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / np.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super(MultiheadAttention, self).__init__()
+        assert (
+            embed_dim % num_heads == 0
+        ), "Embedding dimension must be 0 modulo number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        # Stack all weight matrices 1...h together for efficiency
+        # Note that in many implementations you see "bias=False" which is optional
+        self.qkv_proj = nn.Linear(input_dim, 3 * embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # Original Transformer initialization, see PyTorch documentation
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, embed_dim = x.size()
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3)  # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3)  # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
+
+
+class Predictor(nn.Module):
+    def __init__(self, input_dim, num_heads, dim_feedforward, dropout=0.0):
+        """
+        Inputs:
+            input_dim - Dimensionality of the input
+            num_heads - Number of heads to use in the attention block
+            dim_feedforward - Dimensionality of the hidden layer in the MLP
+            dropout - Dropout probability to use in the dropout layers
+        """
+        super().__init__()
+
+        # Attention layer
+        self.self_attn = MultiheadAttention(input_dim, input_dim, num_heads)
+
+        # Two-layer MLP
+        self.linear_net = nn.Sequential(
+            nn.Linear(input_dim, dim_feedforward),
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_feedforward, input_dim),
+        )
+
+        # Layers to apply in between the main layers
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+        # Attention part
+        attn_out = self.self_attn(x, mask=mask)
+        x = x + self.dropout(attn_out)
+        x = self.norm1(x)
+
+        # MLP part
+        linear_out = self.linear_net(x)
+        x = x + self.dropout(linear_out)
+        x = self.norm2(x)
+
+        return x
+
+
 class SlotAttention(nn.Module):
     def __init__(
         self, num_iterations, num_slots, slot_dim, mlp_hidden_dim, epsilon=1e-8
@@ -45,8 +143,8 @@ class SlotAttention(nn.Module):
         nn.init.xavier_uniform_(self.slots_mu)
         nn.init.xavier_uniform_(self.slots_log_sigma)
 
-    def forward(self, x):
-        b, n, d = x.shape
+    def forward(self, x, slot_initialization=None):
+        b, d = x.shape[0], x.shape[-1]
 
         x = self.input_norm(x)
         keys = self.keys(x)  # [batch_size, input_size, slot_dim]
@@ -55,9 +153,12 @@ class SlotAttention(nn.Module):
         mu = self.slots_mu.expand(b, self.num_slots, -1)
         sigma = self.slots_log_sigma.exp().expand(b, self.num_slots, -1)
 
-        slots = mu + sigma * torch.randn(
-            mu.shape, device=x.device
-        )  # [batch_size, num_slots, slot_dim]
+        if slot_initialization is not None:
+            slots = slot_initialization
+        else:
+            slots = mu + sigma * torch.randn(
+                mu.shape, device=x.device
+            )  # [batch_size, num_slots, slot_dim]
 
         for i in range(self.num_iterations):
             slots_prev = slots
@@ -65,12 +166,12 @@ class SlotAttention(nn.Module):
 
             # Compute attention
             queries = self.queries(slots)  # [batch_size, num_slots, slot_dim]
-            queries *= self.slot_dim ** -0.5  # normalization
 
             # b = batch_size, i = input_size, d = slot_dim, k = num_slots
             attn_logits = torch.einsum(
-                "bid,bkd->bik", keys, queries
+                "bid,bkd->bik", queries, keys
             )  # [batch_size, input_size, num_slots]
+            attn_logits *= self.slot_dim ** -0.5
             attn = F.softmax(attn_logits, dim=1)
 
             # Weighted Mean
@@ -78,7 +179,7 @@ class SlotAttention(nn.Module):
             attn /= torch.sum(attn, dim=-1, keepdim=True)
 
             updates = torch.einsum(
-                "bid,bik->bkd", values, attn
+                "bkd,bik->bid", values, attn
             )  # [batch_size, num_slots, slot_dim]
 
             updates = updates.reshape(-1, d)
@@ -120,20 +221,17 @@ class SoftPositionEmbedding(nn.Module):
         return inputs + grid
 
 
-def spatial_broadcast(z, shape):
+def spatial_broadcast(slots, broadcast_dims):
     """
     Spatial broadcast
 
     Args:
-        z: latents
-        shape: tuple of ints containing width and height of broadcast
+        slots: slots to be broadcasted
+        broadcast_dims: shape to broadcast to
     """
-    z_b = torch.tile(z, (shape[0], shape[1], 1))
-    x = torch.linspace(-1, 1, shape[0])
-    y = torch.linspace(-1, 1, shape[0])
-    x_b, y_b = torch.meshgrid(x, y)
-    z_sb = torch.concatenate([z_b, x_b, y_b], dim=-1)
-    return z_sb
+    slots = slots.reshape((-1, slots.shape[-1]))[:, None, None, :]
+    slots = torch.tile(slots, (1, broadcast_dims[0], broadcast_dims[1], 1))
+    return slots
 
 
 class Encoder(nn.Module):
@@ -145,6 +243,10 @@ class Encoder(nn.Module):
         self.conv3 = nn.Conv2d(hid_dim, hid_dim, kernel_size=5, stride=1, padding=2)
         self.conv4 = nn.Conv2d(hid_dim, hid_dim, kernel_size=5, stride=1, padding=2)
         self.pos_encoder = SoftPositionEmbedding(hid_dim, resolution)
+        self.layer_norm = nn.LayerNorm(hid_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(hid_dim, hid_dim), nn.ReLU(), nn.Linear(hid_dim, hid_dim)
+        )
 
     def forward(self, x):
         x = self.conv1(x)
@@ -158,6 +260,8 @@ class Encoder(nn.Module):
         x = x.permute(0, 2, 3, 1)
         x = self.pos_encoder(x)
         x = torch.flatten(x, 1, 2)
+        x = self.layer_norm(x)
+        x = self.mlp(x)
 
         return x
 
@@ -196,36 +300,73 @@ class Decoder(nn.Module):
         return x
 
 
-class Savi(nn.Module):
+class Initializer(nn.Module):
+    """
+    Provides slot initialization for segmentation mask conditioning signals
+    """
+
+    def __init__(self, hid_dim, resolution):
+        super(Initializer, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, hid_dim, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(hid_dim, hid_dim, kernel_size=5, stride=2)
+        self.conv3 = nn.Conv2d(hid_dim, hid_dim, kernel_size=5, stride=2)
+        self.conv4 = nn.Conv2d(hid_dim, hid_dim, kernel_size=5, stride=1)
+        self.pos_embed = SoftPositionEmbedding(hid_dim, resolution)
+        self.mlp1 = nn.Sequential(
+            nn.Conv2d(hid_dim, hid_dim * 2, kernel_size=1),
+            nn.ReLU(),
+            nn.Conv2d(hid_dim * 2, hid_dim * 2, kernel_size=1),
+        )
+
+        self.mlp2 = nn.Sequential(
+            nn.Linear(hid_dim * 2, hid_dim * 4),
+            nn.ReLU(),
+            nn.Linear(hid_dim * 4, hid_dim * 2),
+        )
+
+        self.layer_norm1 = nn.LayerNorm(hid_dim)
+        self.layer_norm2 = nn.LayerNorm(hid_dim * 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = self.pos_embed(x)
+        x = self.layer_norm1(x)
+        x = self.mlp1(x)
+        x = self.layer_norm2(x)
+        x = self.mlp2(x)
+        return x
+
+
+class SlotAttentionImages(nn.Module):
     def __init__(
         self,
         hid_dim=64,
         resolution=(128, 128),
         num_slots=8,
-        slot_dim=128,
+        slot_dim=64,
         slot_iterations=3,
     ):
-        super(Savi, self).__init__()
+        super(SlotAttentionImages, self).__init__()
 
         self.encoder = Encoder(hid_dim, resolution)
         self.decoder = Decoder(hid_dim, resolution)
-        self.fc1 = nn.Linear(hid_dim, hid_dim)
-        self.fc2 = nn.Linear(hid_dim, hid_dim)
+
         self.slot_attention = SlotAttention(
             slot_iterations, num_slots, slot_dim, slot_dim * 2
         )
 
     def forward(self, image):
         x = self.encoder(image)
-        print(x.shape)
-        x = nn.LayerNorm(x.shape[1:])(x)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
 
         slots = self.slot_attention(x)
         slots = spatial_broadcast(slots, (8, 8))
-
         x = self.decoder(slots)
 
         # Undo combination of slot and batch dimension; split alpha masks.
@@ -241,4 +382,66 @@ class Savi(nn.Module):
         recon_combined = recon_combined.permute(0, 3, 1, 2)
         # `recon_combined` has shape: [batch_size, width, height, num_channels].
 
+        return recon_combined, recons, masks, slots
+
+
+class SlotAttentionVideo(nn.Module):
+    def __init__(
+        self,
+        hid_dim=64,
+        resolution=(128, 128),
+        num_slots=8,
+        slot_dim=64,
+        slot_iterations=3,
+    ):
+        super(SlotAttentionVideo, self).__init__()
+
+        self.encoder = Encoder(hid_dim, resolution)
+        self.decoder = Decoder(hid_dim, resolution)
+        self.initializer = Initializer(hid_dim, resolution)
+        self.predictor = Predictor(hid_dim, 4, 256)
+        self.corrector = SlotAttention(
+            slot_iterations, num_slots, slot_dim, slot_dim * 2
+        )
+
+    def forward(self, images, cues=None):
+        xs = []
+        if cues:
+            slot_initialization = self.initializer(cues)
+        else:
+            slot_initialization = None
+
+        # Encode frames
+        B, T, C, H, W = images.shape
+        preds = []
+        for t in range(T):
+            image = images[:, t]
+            x = self.encoder(image)
+            slots = self.corrector(x, slot_initialization)
+            slot_initialization = self.predictor(slots)
+            slots = spatial_broadcast(slots, (8, 8))
+            x = self.decoder(slots)
+
+            # Undo combination of slot and batch dimension; split alpha masks.
+            recons, masks = x.reshape(
+                image.shape[0], -1, x.shape[1], x.shape[2], x.shape[3]
+            ).split([3, 1], dim=-1)
+            print(recons.shape)
+            # `recons` has shape: [batch_size, num_slots, width, height, num_channels].
+            # `masks` has shape: [batch_size, num_slots, width, height, 1].
+
+            # Normalize alpha masks over slots.
+            masks = nn.Softmax(dim=1)(masks)
+            recon_combined = torch.sum(recons * masks, dim=1)  # Recombine image.
+            recon_combined = recon_combined.permute(0, 3, 1, 2)
+            # `recon_combined` has shape: [batch_size, width, height, num_channels].
+
+            preds.append(
+                {
+                    "recon_combined": recon_combined,
+                    "recons": recons,
+                    "masks": masks,
+                    "slots": slots,
+                }
+            )
         return recon_combined, recons, masks, slots
